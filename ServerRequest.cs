e4a6208642559d2fbd3387cd0abfec7cb7b07e7e
Walter Setti <walter.setti@iit.it>
2020-11-02 13:27:17 +0100

    Cambiamenti alla funzione serverRequest

--- ServerRequest.cs
+++ ServerRequest.cs
@@ -15,6 +15,9 @@ using System.Collections;
 using System.Timers;
 using System.Windows;
 using System.Diagnostics;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Timer = System.Timers.Timer;
 
 namespace UDA_server_communication
@@ -27,6 +30,7 @@ namespace UDA_server_communication
         public int counter;
         public string UDA_index1;
         public string char1;
+        public string save_status;
         ArrayList L = new ArrayList();
         public ServerRequest(Form1 form, string f)
         {
@@ -34,7 +38,7 @@ namespace UDA_server_communication
             UDA_index1 = f;
             counter = 0;
             aTimer = new System.Timers.Timer(10000);
-            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Elapsed += new ElapsedEventHandler(Get_Status_UDA);
             aTimer.Interval = 1000;
             aTimer.Enabled = true;
 
@@ -49,8 +53,8 @@ namespace UDA_server_communication
                 return "";
         }
         
-        //Alberto: cosa vuol dire Server_Request???? dai dei nomi sensati alle cose.
-        // Walter: Questo modulo serve per l'interrogazione con il server. Ho cambiato il suo nome
+        //Alberto: cosa vuol dire Recxmp1???? dai dei nomi sensati alle cose.
+        // Walter: Questo modulo serve per l'interrogazione con il server. Ho cambiato il suo nome in "Server_Request"
         public async static Task<string> Server_Request(string url)
         {
             WebRequest server = HttpWebRequest.Create(url);
@@ -63,53 +67,55 @@ namespace UDA_server_communication
         }
 
 
-        // non ho capito cosa fa questa callback. qui ci devi mettere SOLO la parte di get, periodico.
+        // Alberto: non ho capito cosa fa questa callback. qui ci devi mettere SOLO la parte di get, periodico.
         // che riceve lo status e, solo se è nuovo, lo passa alla form che ha instanziato questa classe.
         // si era detta, ma continui a non ascoltare, che ti saresti tenuto qui in memoria l'ultimo status 
 
-        // i put li metti altrove. questa classe deve esporre il metodo putStatus che setta lo status dell'uda 
-        // e ritorna eventualmente un feedback se è riuscita a mandare l'informazione.
-        public async void OnTimedEvent(object source, ElapsedEventArgs e)
+        //Walter: La callback OneTimeEdvent (adesso Get_Status_UDA) serviva inizialmente per Get e Put. Ho cambiato adesso. 
+        // Questa funzione viene invocata dal timer e serve per ottenere (get) lo stato della UDA selezionata.
+        // Dalla applicazione implementata da Sax, vado a cambiare lo stato della mia UDA tramite il comando PUT.
+        // L'UDA viene interrogata ogni secondo e solo se lo stato cambia viene mandato il messaggio di PUT al server. Esempio:
+        // L'UDA è al tempo 0 nello stato IDLE, quindi lo comunica al server (manda tramite il PUT il suo stato).
+        // Per un pò resta così finchè, dalla applicazione di Sax, mando il messaggio START (k=1). La funzione fa un match 
+        // con l'ultimo stato salvato. Quindi al tempo T(actual)-1, siccome lo stato attuale e quello salvato non corrispondono, 
+        //manda un messaggio al server con che riceverà quindi la stringa relativa a STARTED, visibile sia dall'exe che ho fatto sia dalla applicazione
+        // di Sax. Non so se mi sono spiegato bene, o se intendevi queste modifiche nel commento precedente, magari ho capito
+        // male io. Nel caso sentiamoci. 
+
+        
+        public async void Get_Status_UDA(object source, ElapsedEventArgs e)
         {
-            string url, get_url;
-            //int leng_g;
+            string url, get_url,char_status,char_status1;
             get_url = "https://www.sagosoft.it/_API_/cpim/luda/www/luda_20200901_0900//api/uda/get/?i="+ UDA_index1;
-            //leng_g = get_url.Length;
             try
             {
-                //concatenate_g = string.Concat("i=", UDA_index1);
-                //get_url = get_url.Replace("i=1", concatenate_g);
-                string t = await Server_Request(get_url);
-                main.Show_String(t, 1);              
-                char1 = Convert.ToString(t[88]);    // t[88] cos'è!?!?!?!? se t cambia lunghezza non funziona piu. 
-                                                    // t è la versione string di una struttura dati di tipo JSON. 
-                                                    // tu devi fare il parsing di tale json e prendere il campo giusto.
+                string json_string = await Server_Request(get_url);
+                main.Show_String(json_string, 1);
+                JObject json_parsed = JObject.Parse(json_string);
+                char_status = (string)json_parsed["status"];    //Alberto: t[88] cos'è!?!?!?!? se t cambia lunghezza non funziona piu. 
+                                                                // t è la versione string di una struttura dati di tipo JSON. 
+                                                                // tu devi fare il parsing di tale json e prendere il campo giusto.
+                                                                // Walter: Ho fatto il parsing così da selezionare il campo giusto.
+
+                // La variabile main contatore server per salvare lo stato "attuale" per poi confrontarlo di volta in volta.
+                // Se il contatore è a 0, vuol dire che l'applicazione è appena partita quindi l'UDA si trova già in un suo 
+                // status, dato dalla applicazione di Sax. Il contatore diventa quindi 1, e si passa all'else, all'interno 
+                // del quale, si confronta lo stato attuale con quello precedente, ovvero quello salvato in memoria. 
                 if (main.contatore == 0)
                 {
-                    main.L1.Insert(main.contatore, char1);
-                    url = getPutUrl(char1);
-                    string t1 = await Server_Request(url);
-                    main.Show_String(t1, 2);
-                    main.Status_Changed(char1, 2);
+                    save_status = char_status; 
+                    putStatus_Server(char_status);
+                    main.contatore++;
                 }
                 else
                 {
-
-                    main.L1.Insert(main.contatore, char1);
-                    if (main.L1[main.contatore] == main.L1[main.contatore - 1])
+                    char_status1 = (string)json_parsed["status"];
+                    if (!string.Equals(char_status1, save_status))
                     {
                         main.contatore = 0;
-                    }
-                    else
-                    {
-                        url = getPutUrl(char1);
-                        string t1 = await Server_Request(url);
-                        main.Show_String(t1, 2);
-                        main.Status_Changed(char1, 2);
-                        main.contatore = 1;
+                        putStatus_Server(char_status1);
                     }
                 }
-                main.contatore++;
             }
             catch (Exception ex)
             {
@@ -120,9 +126,22 @@ namespace UDA_server_communication
 
         }
 
-        public int putStatus(int status)
+        //Alberto: Questa classe deve esporre il metodo putStatus che setta lo status dell'uda 
+        // e ritorna eventualmente un feedback se è riuscita a mandare l'informazione.
+
+        //Walter: Il metodo putStatus_Server, serve affinchè l'UDA possa mandare al server il messaggio 
+        //del suo stato, quindi il feedback. Lo status dell'UDA, puoi soltato settarlo dall'applicazione di Sax se non vuoi che l'exe
+        // abbia alcun pulsante di interazione con l'utente.
+        //Quindi, se l'UDA ha per esemprio ricevuto dal server, tramite la stringa PUT il comando "START" (k=1), rimanderà al server la notifica "STARTED"
+        // tramite il put. Il cambio nello stato del server lo si può vedere sia dalla textbox in basso a destra della Form1
+        // sia dall'applicazione di Sax
+        public async void putStatus_Server(string status)
         {
-            return 0;
+            string url_put_server;
+            url_put_server = getPutUrl(status);
+            string server_status = await Server_Request(url_put_server);
+            main.Show_String(server_status, 2);
+            main.Status_Changed(status, 2);
         }
 
     }
